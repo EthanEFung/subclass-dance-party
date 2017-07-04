2f41072dd7db220f9f5fcc294b9d00b2c43316e4
Hack Reactor Student <student@hackreactor.com>
2017-07-03 17:20:46 -0700

    Refractor code

--- src/dancer.js
+++ src/dancer.js
@@ -4,18 +4,19 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   this.timeBetweenSteps = timeBetweenSteps;
+  this.setPosition(top, left);
   this.step();
   // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
+  
 
   
 };
 
-makeDancer.prototype.step = function(input, timeStep) {
+makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  
-  setTimeout(input, timeStep );
+  console.log(this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
