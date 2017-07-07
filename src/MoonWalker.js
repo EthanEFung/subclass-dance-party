6f7fa94a20f7e3c46fb4908fbaea8a1331cd754f
Hack Reactor Student <student@hackreactor.com>
2017-07-06 17:32:57 -0700

    Write operating MoonWalker
    With the help of Michael Dai, learned about
    jQuery animate which manipulates a css class
    continuously.  Will do more research in the
    reasons why.

--- src/MoonWalker.js
+++ src/MoonWalker.js
@@ -7,4 +7,5 @@ MoonWalker.prototype.constructor = MoonWalker;
 
 MoonWalker.prototype.step = function() {
   makeDancer.prototype.step.call(this);
+  this.$node.animate({left : "+=50px"}, this.timeBetweenSteps);
 };
